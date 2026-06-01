7621c551cca764195fd7a48a5f6396f19b520e3d
snehalthombare08 <snehalthombare888@gmail.com>
2026-06-01 21:31:38 +0530

    fix: allow all CORS origins

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="CP Analyzer API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
